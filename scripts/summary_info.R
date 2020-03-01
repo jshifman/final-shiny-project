293207d160fcdf71342baf1e40e2d8aa875eb2fc
Carson Bryant <carsonbryant@Carsons-MacBook-Pro.local>
2020-03-01 15:13:10 -0800

    one function

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -5,42 +5,38 @@ lintr::lint("summary_info.R")
 
 # 3-pointer stats:
 # TP - 3 point fields made, TPA - 3 point attempts, TPP - 3 point percentage
+# TP & TPA from the NBA championship for given year & Average # pts a team
+# scored in a single round of the NBA championship
 
-# TP & TPA from the NBA championship for given year
-tpa_and_tp <- function(champs, runnerup, year) {
+tpa_tp_pts <- function(champs, runnerup, year1, year2) {
   ret <- list()
   tpa_champs <- champs %>%
     select(Year, TPA) %>%
-    filter(Year == year) %>%
+    filter(Year == year1) %>%
     select(TPA) %>%
     summarise(total = sum(TPA)) %>%
     pull()
   tpa_runnerups <- runnerup %>%
     select(Year, TPA) %>%
-    filter(Year == year) %>%
+    filter(Year == year1) %>%
     select(TPA) %>%
     summarise(total = sum(TPA)) %>%
     pull()
   ret$tpa <- tpa_champs + tpa_runnerups
   tp_champs <- champs %>%
     select(Year, TP) %>%
-    filter(Year == year) %>%
+    filter(Year == year1) %>%
     select(TP) %>%
     summarise(total = sum(TP)) %>%
     pull()
   tp_runnerups <- runnerup %>%
     select(Year, TP) %>%
-    filter(Year == year) %>%
+    filter(Year == year1) %>%
     select(TP) %>%
     summarise(total = sum(TP)) %>%
     pull()
   ret$tp <- tp_champs + tp_runnerups
-  return(ret)
-}
 
-# Average # pts a team scored in a single round of the NBA championship
-total_points <- function(champs, runnerup, year1, year2) {
-  ret <- list()
   pts_champs_total <- champs %>%
     select(Year, PTS) %>%
     filter(Year >= year1 & Year <= year2) %>%
@@ -59,8 +55,12 @@ total_points <- function(champs, runnerup, year1, year2) {
     select(Year, PTS) %>%
     filter(Year >= year1 & Year <= year2) %>%
     nrow()
-  ret$pts <- round(((pts_champs_total / pts_champs_rows)
-  + (pts_runnerups_total / pts_runnerups_rows))
-  / 2, digits = 0)
+  ret$pts <- round(
+    ((pts_champs_total / pts_champs_rows) +
+      (pts_runnerups_total / pts_runnerups_rows))
+    / 2,
+    digits = 0
+  )
+
   return(ret)
 }
