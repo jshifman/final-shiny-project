317e3b1e90196129834c0f1599b1a7349db4f5a1
Joseph Shifman <jshifman@uw.edu>
2020-03-01 21:27:06 -0800

    completed table and summary

--- scripts/table_summary.R
+++ scripts/table_summary.R
@@ -7,13 +7,13 @@ summary_table <- function(df){
   tab <- df %>%
     group_by(Year) %>%
     summarise(
-      fg_percentage = mean(FGP, na.rm = TRUE),
+      fg_percentage = sum(FG, na.rm = TRUE) / sum(FGA, na.rm = TRUE),
       fg_attempted = sum(FGA, na.rm = TRUE),
       fg_total_points = sum(FG, na.rm = TRUE) * 2,
-      ft_percentage = mean(FTP, na.rm = TRUE),
+      ft_percentage = sum(FT, na.rm = TRUE) / sum(FTA, na.rm = TRUE),
       ft_attempted = sum(FTA, na.rm = TRUE),
       ft_total_points = sum(FT, na.rm = TRUE),
-      tp_percentage = mean(TPP, na.rm = TRUE),
+      tp_percentage = sum(TP, na.rm = TRUE) / sum(TPA, na.rm = TRUE),
       tp_attempted = sum(TPA, na.rm = TRUE),
       tp_total_points = sum(TP, na.rm = TRUE) * 3
     )
